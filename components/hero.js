f49e1dc6a592ce633e8493f6b933e3b21baccb3c
javidnoshadi <javid.king63@gmail.com>
2021-01-02 11:50:47 +0330

    dropping the repo. got a urgent project

--- components/hero.js
+++ components/hero.js
@@ -1,8 +1,14 @@
 const Hero = ()=>{
     return(<div className='Hero'>
-      <div>
+      <div className='hero-text'>
       <h1>چرا به وب سایت نیاز دارید<span className='blink'> ؟ </span></h1>
                                         <p>الان میگم چرا</p>
+
+      </div>
+      <img className='rec1 rec' src='./asses/rec1.svg' />
+      <img className='rec2 rec' src='./asses/rec1.svg' />
+      <div className='bottom'>
+       <img src='./asses/web1.svg' />
       </div>
     </div>)
 }
